f43c57cafefd4ab726daa9443010aa2aede13c8f
Alex K <slantz@users.noreply.github.com>
2016-12-28 11:48:41 +0200

    adding mongoose session to express session (#4)

--- index.js
+++ index.js
@@ -7,15 +7,25 @@ var path = require('path');
 var join = require('path').join;
 
 var express = require('express');
+var session = require('express-session');
 var app = express();
 
 var compress = require('compression');
+var cors = require('cors');
 var layouts = require('express-ejs-layouts');
 var passport = require('passport');
+const upload = require('multer')();
+
+const mongoStore = require('connect-mongo')(session);
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 var mongoose = require('mongoose');
 
 const models = join(__dirname, 'server/api/models');
+const pkg = require('./package.json');
 
 // Bootstrap models
 fs.readdirSync(models)
@@ -31,8 +41,47 @@ app.set('view options', {layout: 'layout'});
 app.set('views', path.join(process.cwd(), '/server/views'));
 
 app.use(compress());
+app.use(cors());
 app.use(layouts);
 app.use('/client', express.static(path.join(process.cwd(), '/client')));
+
+// expose package.json to views
+app.use(function (req, res, next) {
+    res.locals.pkg = pkg;
+    res.locals.env = env;
+    next();
+});
+
+// bodyParser should be above methodOverride
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.single('image'));
+app.use(methodOverride(function (req) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
+
+// CookieParser should be above session
+app.use(cookieParser());
+app.use(cookieSession({ secret: 'secret' }));
+
+connect()
+    .on('error', console.log)
+    .on('disconnected', connect)
+    .once('open', listen);
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: pkg.name,
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -66,11 +115,6 @@ app.get(/^\/.*(?!(auth|api)).*$/, function(req, res) {
     user: req.user
   });
 });
-
-connect()
-    .on('error', console.log)
-    .on('disconnected', connect)
-    .once('open', listen);
 
 var port = Number(process.env.PORT || 3001);
 

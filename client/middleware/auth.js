20fce9966550a50b7e68c6e9acbe8549377aed83
Alex Kobylinskyi <oleksii.kobylinskyi@opower.com>
2016-12-29 13:39:45 +0200

    added simple auth middleware and new route structure

---
+++ client/middleware/auth.js
@@ -0,0 +1,13 @@
+export const REQUIRE_LOGIN = (nextState, replace, cb) => {
+    function checkAuth() {
+        console.log(nextState);
+        const { user } = nextState;
+        if (!user) {
+            // oops, not logged in, so can't be here!
+            replace('/');
+        }
+        cb();
+    }
+
+    checkAuth();
+};

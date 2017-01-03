2f017eaf3425205196c1aed5e97c515ad5d7d770
Alex Kobylinskyi <oleksii.kobylinskyi@opower.com>
2017-01-03 17:49:33 +0200

    added basic models

--- server/api/models/Bakery.js
+++ server/api/models/Bakery.js
@@ -1,3 +1,42 @@
 /**
  * Created by alexkobylinskyi on 12/27/16.
  */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+/**
+ * User Schema
+ */
+
+const BakerySchema = new Schema({
+    category: {
+        type: String,
+        default: ''
+    },
+    weight: {
+        type: Number,
+        default: null
+    },
+    ingredients: [{
+        type: mongoose.Schema.ObjectId,
+        ref: 'Ingredient'
+    }],
+    imgUrl: {
+        type: String,
+        default: ''
+    },
+    numberOfPieces: {
+        type: Number,
+        default: 1
+    }
+});
+
+mongoose.model('Bakery', BakerySchema);
---
+++ server/api/models/Ingredient.js
@@ -0,0 +1,42 @@
+/**
+ * Created by alexkobylinskyi on 1/3/17.
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+/**
+ * User Schema
+ */
+
+const IngredientSchema = new Schema({
+    type: {
+        type: String,
+        default: ''
+    },
+    taste: {
+        type: String,
+        default: ''
+    },
+    color: {
+        type: String,
+        default: ''
+    },
+    substance: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Number,
+        default: null
+    }
+});
+
+mongoose.model('Ingredient', IngredientSchema);

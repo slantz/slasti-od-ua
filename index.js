a631d429f0347817a3a87714712afe9d94dfe385
Alex Kobylinskyi <oleksii.kobylinskyi@oracle.com>
2017-04-04 18:16:27 +0300

    added basic mailer

--- index.js
+++ index.js
@@ -275,6 +275,35 @@ function connect () {
     return mongoose.connect(process.env.DB, options).connection;
 }
 
+
+var nodemailer = require('nodemailer');
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: '',
+        pass: ''
+    }
+});
+
+// setup email data with unicode symbols
+let mailOptions = {
+    from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
+    cc: '', // list of receivers
+    to: '',
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world ?', // plain text body
+    html: '<b>Hello world ?</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+});
+
 if (env.production === false) {
   var webpack = require('webpack');
   var WebpackDevServer = require('webpack-dev-server');
